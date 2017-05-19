2eb29e7fa26110fdb1fe2f3ac5f0c0cc957963ec
Jacob Simpson <jacob.r.a.simpson@gmail.com>
2017-05-19 10:31:52 +1000

    Added Magnetometer (HMC5883L)
    drivers/compass/compass_hmc5883l.c

--- src/main/target/IRCFUSIONF3/target.mk
+++ src/main/target/IRCFUSIONF3/target.mk
@@ -5,5 +5,6 @@ TARGET_FLAGS = -DSPRACINGF3
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_mpu6050.c \
-            drivers/barometer/barometer_bmp085.c
+            drivers/barometer/barometer_bmp085.c \
+            drivers/compass/compass_hmc5883l.c
 
